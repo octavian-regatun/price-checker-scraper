c1740d70cfc0160ac9f06e9f01ce2c96f5def5fc
Octavian Regatun <octavian.regatun@outlook.com>
2020-06-24 16:32:23 +0300

    travis test fix #14

--- src/tests/pcgarage.test.js
+++ src/tests/pcgarage.test.js
@@ -23,23 +23,13 @@ describe('Tests PcGarage Extractor', () => {
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
 
-    const client = await page.target().createCDPSession();
-
-    // Set throttling property
-    await client.send('Network.emulateNetworkConditions', {
-      offline: false,
-      downloadThroughput: (750 * 1024) / 8,
-      uploadThroughput: (250 * 1024) / 8,
-      latency: 100
-    });
-
-    await page.setViewport({ width: 1920, height: 900 });
     await page.goto(`${URL}/${search}`);
 
-    // await page.waitForSelector(
-    //   '#listing-right > div.grid-products.clearfix.product-list-container > div:nth-child(1) > div > div.pb-specs-container > div.pb-name > a',
-    //   { timeout: 0 }
-    // );
+    console.log(`${URL}/${search}`);
+
+    let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+
+    console.log(bodyHTML);
 
     const name = await page.$eval(
       '#listing-right > div.grid-products.clearfix.product-list-container > div:nth-child(1) > div > div.pb-specs-container > div.pb-name > a',
