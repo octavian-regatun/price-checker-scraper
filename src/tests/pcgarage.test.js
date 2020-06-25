695ef25c45072017b99205d5eb579d0d5f7d4fd9
Octavian Regatun <octavian.regatun@outlook.com>
2020-06-26 01:23:11 +0300

    rewrite test with cheerio instead of puppeteer

--- src/tests/pcgarage.test.js
+++ src/tests/pcgarage.test.js
@@ -1,51 +1,39 @@
 require('dotenv').config();
 
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('@bengo923/puppeteer-extra-plugin-stealth');
+const delay = require('delay');
 
-puppeteer.use(StealthPlugin());
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
 
 const PcGarage = require('../extractors/pcgarage');
 
-const options = {
-  headless: process.env.HEADLESS === 'true',
-  slowMo: process.env.SLOWMO || 0,
-  args: ['--no-sandbox --disable-setuid-sandbox']
-};
+const { URL } = PcGarage;
 
-const URL = 'https://www.pcgarage.ro/cauta';
-
-describe('Tests PcGarage Extractor', () => {
-  beforeEach(() => {
-    jest.setTimeout(120000);
+describe('PcGarage extractor tests', () => {
+  afterEach(() => {
+    delay(10000);
   });
 
-  test('it tests if the API extracts the name from the site', async (done) => {
-    const search = 'rtx 2060 super';
+  test('it tests if the API extracts the name from the site', async () => {
+    const search = 'telefon';
 
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
+    let $;
+    try {
+      $ = await fetch(`${URL}/${search}`, { method: 'GET' })
+        .then((response) => response.text())
+        .then((body) => cheerio.load(body));
+    } catch (error) {
+      console.log(error);
+    }
 
-    await page.goto(`${URL}/${search}`);
+    const firstProductName = $(
+      '#listing-right > div.grid-products.clearfix.product-list-container > div:nth-child(1) > div > div.pb-specs-container > div.pb-name > a'
+    ).attr('title');
 
-    console.log(`${URL}/${search}`);
-
-    let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-
-    console.log(bodyHTML);
-
-    const name = await page.$eval(
-      '#listing-right > div.grid-products.clearfix.product-list-container > div:nth-child(1) > div > div.pb-specs-container > div.pb-name > a',
-      (element) => element.getAttribute('title')
-    );
-
-    console.log(name);
+    await delay(10000);
 
     const PcGarageResponse = await PcGarage.request(search);
 
-    expect(PcGarageResponse.products[0].name).toBe(name);
-
-    await browser.close();
-    done();
+    expect(PcGarageResponse.products[0].name).toBe(firstProductName);
   }, 120000);
 });
