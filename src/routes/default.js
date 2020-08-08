8e37fbfd901ddecdb8c3b3715950df3ef62c6a0a
Octavian Regatun <octavian.regatun@outlook.com>
2020-08-08 20:05:57 +0300

    clean up code

--- src/routes/default.js
+++ src/routes/default.js
@@ -1,39 +1,51 @@
+const CustomLog = require('../utils/CustomLog');
+const store = require('store');
+const Scraper = require('../scraper');
+const { ACTIONS } = require('../config');
+const delay = require('delay');
+
 module.exports = (app) => {
-  app.post('/', (req, res) => {
+  app.post('/', async (req, res) => {
+    CustomLog.clearLogs();
+
     const CONFIGS = req.body;
-    const CustomLog = require('../utils/CustomLog');
 
-    (async () => {
-      const Scraper = require('../scraper');
-      const { ACTIONS, IS_CONFIG_VALID } = require('../config');
-      const delay = require('delay');
+    require('../utils/validateConfig')(CONFIGS, res);
 
-      require('../utils/validateConfig')(CONFIGS, res);
-
-      if (IS_CONFIG_VALID) {
-        for (const [index, config] of CONFIGS.entries()) {
-          CustomLog.info(
-            `Scraper is doing config ${index + 1}/${CONFIGS.length}`
-          );
-
-          const provider = config.provider;
-          const action = config.action;
-          const search = config.search;
-          const category = config.category;
-
-          if (action === ACTIONS.addByCategory) {
-            await Scraper[action](category, provider);
-          } else {
-            await Scraper[action](search, provider);
-          }
-
-          await delay(2000);
-        }
-      }
-    })().then(() => {
-      CustomLog.successful('Scraper ran the configs!');
-      res.send(CustomLog.LOGS);
-      CustomLog.clearLogs();
+    await runScraper(CONFIGS).then(() => {
+      scraperRanSuccessfully(res);
     });
   });
 };
+
+async function runScraper(CONFIGS) {
+  if (isConfigValid()) {
+    for (const [index, config] of CONFIGS.entries()) {
+      CustomLog.info(`Scraper is doing config ${index + 1}/${CONFIGS.length}`);
+
+      const provider = config.provider;
+      const action = config.action;
+      const search = config.search;
+      const category = config.category;
+
+      if (action === ACTIONS.addByCategory) {
+        await Scraper[action](category, provider);
+      } else {
+        await Scraper[action](search, provider);
+      }
+
+      await delay(2000);
+    }
+  }
+}
+
+function scraperRanSuccessfully(res) {
+  if (isConfigValid()) {
+    CustomLog.successful('Scraper ran the configs!');
+    res.send(CustomLog.LOGS);
+  }
+}
+
+function isConfigValid() {
+  return store.get('isConfigValid');
+}
